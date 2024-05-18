da981f3f1da0e3774e021af217a6eb83a18b3892
Kamal <kamalacca@gmail.com>
2024-05-18 19:54:52 +0400

    add document viewer block using create-blcok package

---
+++ document-viewer/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '685c235c8a51c501b059');
---
+++ document-viewer/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ec6275a2fe416390da7c');
