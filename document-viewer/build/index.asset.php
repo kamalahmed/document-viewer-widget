a6f4feda5992362b703a2ffe0c1d4cf372534e6e
Kamal <kamalacca@gmail.com>
2024-05-21 20:18:41 +0400

    remove repeated style from editor css

--- document-viewer/build/index.asset.php
+++ document-viewer/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f81b856d54bccf3c9668');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a6cfde659e1b763a3ee5');
